aa6a5750e584d6b302a304a937c8fc41c06e24f9
JeongDH <wjdehgns97@naver.com>
2021-12-03 19:56:12 +0900

    에러 제거

--- WPF 팀프로젝트/AcceptanceList.xaml.cs
+++ WPF 팀프로젝트/AcceptanceList.xaml.cs
@@ -21,41 +21,35 @@ namespace WPF_팀프로젝트 {
         public AcceptanceList() {
             InitializeComponent();
 
-            DataGridView.DataSource = DataManager.Accepts;
+            //DataGridView.DataSource = DataManager.Accepts;
             DataGridView.CurrentCellChanged += DataGridView_CurrentCellChanged;
 
         }
 
-
-            private void Button_Click(object sender, RoutedEventArgs e) // 입장
-            {
-                try
-                {
-                    DataGridView.DataSource = null;
-                    DataGridView.DataSource = DataManager.Accepts;
-                    DataManager.Save();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("입장에 실패하셨습니다........");
-                }
-            }
-            private void Ibiinhugwa_Selected(object sender, RoutedEventArgs e) // 콤보 박스에 이비인후과 선택 시
-            {
-                DataGridView
-            }
-
-        private void DataGridView_CurrentCellChanged(object sender, EventArgs e)
+        private void Button_Click( object sender, RoutedEventArgs e ) // 입장
         {
-            Acceptance acceptance = DataGridView.DataBoundItem as Acceptance;
+            try {
+                // DataGridView.DataSource = null;
+                // DataGridView.DataSource = DataManager.Accepts;
+                DataManager.Save();
+            }
+            catch ( Exception ex ) {
+                MessageBox.Show("입장에 실패하셨습니다........");
+            }
+        }
+        private void Ibiinhugwa_Selected( object sender, RoutedEventArgs e ) // 콤보 박스에 이비인후과 선택 시
+        {
+            //DataGridView;
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (DepartmentBox.SelectedItem.ToString() == "이비인후과") { DataGridView.ItemsSource = DataManager.Accepts.Where(x => x.Department == "이비인후과").ToList(); }
-            if (DepartmentBox.SelectedItem.ToString() == "정형외과") { DataGridView.ItemsSource = DataManager.Accepts.Where(x => x.Department == "정형외과").ToList(); }
-            if (DepartmentBox.SelectedItem.ToString() == "내과") { DataGridView.ItemsSource = DataManager.Accepts.Where(x => x.Department == "내과").ToList(); }
+        private void DataGridView_CurrentCellChanged( object sender, EventArgs e ) {
+            //Acceptance acceptance = DataGridView.DataBoundItem as Acceptance;
+        }
+
+        private void ComboBox_SelectionChanged( object sender, SelectionChangedEventArgs e ) {
+            if ( DepartmentBox.SelectedItem.ToString() == "이비인후과" ) { DataGridView.ItemsSource = DataManager.Accepts.Where(x => x.Department == "이비인후과").ToList(); }
+            if ( DepartmentBox.SelectedItem.ToString() == "정형외과" ) { DataGridView.ItemsSource = DataManager.Accepts.Where(x => x.Department == "정형외과").ToList(); }
+            if ( DepartmentBox.SelectedItem.ToString() == "내과" ) { DataGridView.ItemsSource = DataManager.Accepts.Where(x => x.Department == "내과").ToList(); }
         }
     }
-    }
-
+}
--- WPF 팀프로젝트/Vaccine.xaml.cs
+++ WPF 팀프로젝트/Vaccine.xaml.cs
@@ -21,59 +21,36 @@ namespace WPF_팀프로젝트 {
     public partial class Vaccine : Page {
         public Vaccine() {
             InitializeComponent();
+        }
+        private void PreviewMouseUp( object sender, MouseButtonEventArgs e ) {
+            DateTime dt = (DateTime)calendar.SelectedDate;
+            textblock5.Text = dt.ToString("dd");
+            textblock7.Text = dt.ToString("dd");
+        }
 
-            //textblock1.Text = DataManager.Customers
+        private void button1_Click( object sender, RoutedEventArgs e ) {
+            textblock6.Text = "화이자";
+        }
 
-            //Customer customer = DataManager.Customers.Single(x => x.cID == textblock1.Text);
-            //Vaccination vaccinations = DataManager.Vaccinations.Single(x => x.Count.ToString() == textblock9.Text); // 수량을 string..?
+        private void button2_Click( object sender, RoutedEventArgs e ) {
+            textblock6.Text = "모더나";
+        }
 
-            //customer.cID = textblock1.Text;
-            //            customer.Name = textblock2.Text;
-            //            customer.Birth = textblock3.Text;
-            //            customer.Phone = textblock4.Text;
+        private void button3_Click( object sender, RoutedEventArgs e ) {
+            textblock6.Text = "AZ";
+        }
 
-            //            DataManager.Save();
-            /*
-            private void PreviewMouseUp(object sender, MouseButtonEventArgs e) // 날짜 누르면 텍스트 블럭에 날짜 뜨게
-            {
-                DateTime dt = (DateTime)calendar.SelectedDate;
-                textblock5.Text = dt.ToString("dd");
-                textblock7.Text = dt.ToString("dd");
-            }
+        private void button4_Click( object sender, RoutedEventArgs e ) {
+            textblock6.Text = "얀센";
+       
+        }
 
-            private void button1_Click(object sender, RoutedEventArgs e) // 화이자 버튼 클릭 시 텍스트 블럭에 화이자 뜨게
-            {
-                textblock6.Text = "화이자";
+        private void button5_Click( object sender, RoutedEventArgs e ) {
 
-            }
+        }
 
-            private void button2_Click(object sender, RoutedEventArgs e) // 모더나 버튼 클릭 시 텍스트 블럭에 모더나 뜨게
-            {
-                textblock6.Text = "모더나";
-            }
+        private void button6_Click( object sender, RoutedEventArgs e ) {
 
-            private void button3_Click(object sender, RoutedEventArgs e) // AZ 버튼 클릭 시 텍스트 블럭에 AZ 뜨게
-            {
-                textblock6.Text = "AZ";
-            }
-
-            private void button4_Click(object sender, RoutedEventArgs e) // 얀센 버튼 클릭 시 텍스트 블럭에 얀센 뜨게
-            {
-                textblock6.Text = "얀센";
-            }
-
-            private void button5_Click(object sender, RoutedEventArgs e) // 예약하기
-            {
-
-
-                }
-
-            }
-
-            private void button6_Click(object sender, RoutedEventArgs e) // 취소하기
-            {
-
-            */
         }
     }
 }
